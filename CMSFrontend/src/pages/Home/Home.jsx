36628ec43fec3291a1d3415b45840651b32812cc
milanbhandari047 <milanbhandari047@gmail.com>
2023-11-06 13:14:40 +0545

    api integration

--- CMSFrontend/src/pages/Home/Home.jsx
+++ CMSFrontend/src/pages/Home/Home.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../component/Navbar/Navbar'
 import "./Home.css"
+import axios  from "axios"
+
 const Home = () => {
+
+const[blogs , setBlogs] =useState([])
+  // api call here
+  const fetchBlogs = async()=>{
+    const response = await axios.get("http://localhost:3000/blogs")
+    setBlogs(response.data.blogs)
+
+  }
+
+
+  useEffect(()=>{
+  fetchBlogs()
+  },[])
+
   return (
     <>
       <Navbar/>
     
+  
       <div className="card">
-      <div className="card-content">
-        <h2 className="card-title">Beautiful Card</h2>
-        <h3 className="card-subtitle">A Subtitle</h3>
+      {blogs.map((blogs)=>{
+        return(
+   
+      <div key={blogs._id} className="card-content">
+        <h2 className="card-title">{blogs.title}</h2>
+        <h3 className="card-subtitle">{blogs.subTitle}</h3>
         <p className="card-description">
-          This is a beautiful card with a title, Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, harum quaerat sit vitae enim atque omnis, sint quod incidunt vel eos maiores distinctio ipsam repudiandae corrupti delectus unde optio autem. subtitle, description, and a button. You can add your content here.
-        </p>
+           {blogs.description} </p>
         <button className="card-button">Click Me</button>
       </div>
-    </div>
+   
+        )
+      })}
+        </div>
+
+
+
 
 
 
